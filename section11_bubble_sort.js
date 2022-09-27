214f1681295b1a225160b5f4d9d870d499120a7b
Roy Daniel <roydanielr@gmail.com>
2022-09-27 09:25:17 -0400

    bubble sort optimization

--- section11_bubble_sort.js
+++ section11_bubble_sort.js
@@ -1,13 +1,17 @@
 const bubbleSort = (arr) => {
+    let  noSwapsOnLastIteration
     for(var i = arr.length; i > 0; i--){
-      for(var j = 0; j < i - 1; j++){
-        console.log(arr, arr[j], arr[j+1]);
-        if(arr[j] > arr[j+1]){
-          var tmp = arr[j];
-          arr[j] = arr[j+1];
-          arr[j+1] = tmp;         
+        noSwapsOnLastIteration = true
+        for(var j = 0; j < i - 1; j++){
+            console.log(arr, arr[j], arr[j+1])
+            if(arr[j] > arr[j+1]){
+                var tmp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = tmp
+                noSwapsOnLastIteration = false      
+            }
         }
-      }
+        if(noSwapsOnLastIteration) break
     }
-    return arr;
+    return arr
   }
