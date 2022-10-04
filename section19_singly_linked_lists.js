81f8814e9ebf5a3ad5ae120957783533fb7ebf5a
Roy Daniel <roydanielr@gmail.com>
2022-10-04 10:20:04 -0400

    reverse method

--- section19_singly_linked_lists.js
+++ section19_singly_linked_lists.js
@@ -38,8 +38,8 @@ class SinglyLinkedList {
         this.tail = null
         this.length = 0
     }
-    // push:    add new node to end
-    // ============================
+    // 1) push:    add new node to end
+    // ===============================
     push(val) {
         const newNode = new Node(val)
         if (!this.head) /* edge case: empty list */{
@@ -52,8 +52,8 @@ class SinglyLinkedList {
         this.length++
         return this
     }
-    // pop:     remove node from end
-    // ============================
+    // 2) pop:     remove node from end
+    // ================================
     pop() {
         if (!this.head || this.length === 0) return undefined // edge case
         let currNode = this.head
@@ -68,8 +68,8 @@ class SinglyLinkedList {
         if (this.length === 0) this.head = this.tail = null // reset empty list
         return currNode
     }
-    // shift:   remove node from beginning
-    // ===================================
+    // 3) shift:   remove node from beginning
+    // ======================================
     shift() {
         if(!this.head) return undefined // edge case: empty list
         let newHead = this.head.next
@@ -78,8 +78,8 @@ class SinglyLinkedList {
         if (this.length === 0) this.tail = null
         return this.head
     }
-    // unshift: add new node to beginning
-    // ==================================
+    // 4) unshift: add new node to beginning
+    // =====================================
     unshift(val) {
         let newNode = new Node(val)
         if (!this.head) this.head = this.tail = newNode
@@ -88,18 +88,18 @@ class SinglyLinkedList {
         this.length++
         return this
     }
-    // get:     return "idx" of node n
-    // ===============================
+    // 5) get:     return "idx" of node n
+    // ==================================
     get(idx) {
-        if (idx > this.length) return null // edge case: out of bounds
+        if (idx < 0 || idx > this.length) return null // edge case: out of bounds
         let currNode = this.head
-        for (let i = 1; i < idx; i++) {
+        for (let i = 0; i < idx; i++) {
             currNode = currNode.next
         }
         return currNode
     }
-    // set:     change val of "idx" of node n 
-    // ======================================
+    // 6) set:     change val of "idx" of node n 
+    // =========================================
     set(idx, val) {
         let nodeToChange = this.get(idx)
         if (nodeToChange) {
@@ -108,8 +108,8 @@ class SinglyLinkedList {
         }
         return false
     }  
-    // insert:  add node at "idx" n
-    // ============================
+    // 7) insert:  add node at "idx" n
+    // ===============================
     insert(idx, val) {
         if (idx < 0 || idx > this.length) return false // edge case: out of bounds
         if (idx === this.length) return !!this.push(val) // edge case: last item
@@ -121,8 +121,8 @@ class SinglyLinkedList {
         this.length++
         return true
     }
-    // remove:  remove node at "idx" n
-    // ===============================
+    // 8) remove:  remove node at "idx" n
+    // ==================================
     remove(idx) {
         if (idx < 0 || idx > this.length) return undefined // edge case: out of bounds
         if (idx === 0) return this.shift() // edge case: first item
@@ -133,25 +133,47 @@ class SinglyLinkedList {
         this.length--
         return removedNode
     }
-    // reverse: reverse ndoe list order
-    // ================================
+    // 9) reverse: reverse ndoe list order (IN PLACE - no copying)
+    // ===========================================================
+    reverse () {
+        // Step 1 - swap head & tail
+        let currNode = this.head
+        this.head = this.tail
+        this.tail = currNode
+        // Step 2 - iterate and change direction
+        let nextNode, prevNode = null
+        for (let i = 0; i < this.length; i++) {
+            nextNode = currNode.next
+            currNode.next = prevNode
+            prevNode = currNode
+            currNode = nextNode
+        }
+        return this
+    }
+    // 10) print:  aux function that prints list (as an array for simplicity)
+    // ======================================================================
+    print() {
+        const arr = []
+        let currNode = this.head
+        while (currNode) {
+            arr.push(currNode.val)
+            currNode = currNode.next
+        }
+        console.log(arr)
+    }
 }
 
 const list = new SinglyLinkedList()
-list.push(1)
-list.push(2)
-list.push(3)
-// console.log(list.head)      //  Node { val: 1, next: Node { val: 2, next: null } }
-// console.log(list.head.next) //  Node { val: 2, next: Node { val: 3, next: null } }
-// console.log(list.tail)      //  Node { val: 3, next: null }
-// console.log(list.pop())
-// console.log(list)
-// console.log(list.shift())
-// console.log(list.unshift(4))
-// console.log(list.set(3, 5))
-// console.log(list.get(3))
-console.log(list.insert(2,4))
-console.log(list.length)
-console.log(list.remove(1))
-console.log(list.get(2))
-// console.log()
+list.push(1)                // 1
+list.push(2)                // 1 - 2
+list.push(3)                // 1 - 2 - 3
+list.pop()                  // 1 - 2
+list.shift()                // 2
+list.unshift(4)             // 4 - 2
+console.log(list.get(2))    // Node { val: 2, next: null }
+list.push(3)                // 4 - 2 - 3
+list.set(2, 5)              // 4 - 2 - 5
+list.insert(2,6)            // 4 - 2 - 6 - 5
+list.remove(1)              // 4 - 6 - 5
+list.reverse()              // 5 - 6 - 4
+list.print()
