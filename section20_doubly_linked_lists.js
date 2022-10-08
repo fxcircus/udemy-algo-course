7486307c134a6136b6fb6537eca0fab1000444dd
Roy Daniel <roydanielr@gmail.com>
2022-10-08 17:34:49 -0400

    unshift method

--- section20_doubly_linked_lists.js
+++ section20_doubly_linked_lists.js
@@ -74,6 +74,19 @@ class DoublyLinkedList {
         this.length--
         return oldHead
     }
+    unshift(val){
+        var newNode = new Node(val)
+        if(this.length === 0) {
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.head.prev = newNode
+            newNode.next = this.head
+            this.head = newNode
+        }
+        this.length++
+        return this
+    }
 }
 
 const list = new DoublyLinkedList()
