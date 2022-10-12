178eba11ecefcb2aafc247828273dc7dbe2979f0
Roy Daniel <roydanielr@gmail.com>
2022-10-12 15:39:02 -0400

    Tree summary

---
+++ section22_binary_search_trees.js
@@ -0,0 +1,30 @@
+/* Binary Search Tree:
+======================
+-   A nonlinear data structure, that consists of nodes in a parent-child relationships
+-   Examples: HTML DOM, AI models, JSON, Folders in an OS
+
+Terminology:
+============
+Root:       the top node in a tree
+Child:      a node directly connected to another node when moving away from the root
+Parent:     The node before the child. What ther child connects to...
+Siblings:   A group of nodes with the same parents
+Leaf:       A node with no children
+Edge:       The connection between the nodes
+
+Example:
+========
+    ________1________
+    |               |
+____5____       ____12
+|       |       |
+3       6       11
+
+
+Big O time complexity:
+======================
+Insertion   
+Removal     
+Searching   
+Access      
+*/
