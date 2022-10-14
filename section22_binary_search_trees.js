5d06ff46506c1c7e6f26ec17940d797bbf9abca3
Roy Daniel <roydanielr@gmail.com>
2022-10-14 08:56:44 -0400

    manual insert example

--- section22_binary_search_trees.js
+++ section22_binary_search_trees.js
@@ -1,19 +1,19 @@
 /* Binary Search Tree:
 ======================
--   A nonlinear data structure, that consists of nodes in a parent-child relationships
+-   A nonlinear data structure, that consists of nodes in a parent-child relationship
 -   Parent nodes have <= 2 children
 -   Left child node < parent
 -   Right child node > parent
--   Common use cases: HTML DOM, AI models, JSON, Folders in an OS
+-   Common use cases: HTML DOM, AI models, JSON, OS Folders structure
 
 Terminology:
 ============
 Root:       the top node in a tree
 Child:      a node directly connected to another node when moving away from the root
-Parent:     The node before the child. What ther child connects to...
-Siblings:   A group of nodes with the same parents
-Leaf:       A node with no children
-Edge:       The connection between the nodes
+Parent:     the node before the child. What ther child connects to...
+Siblings:   a group of nodes with the same parents
+Leaf:       a node with no children
+Edge:       the connection between the nodes
 
 Example:
 ========
@@ -31,3 +31,34 @@ Removal
 Searching   
 Access      
 */
+class Node {
+    constructor(val) {
+        this.val = val
+        this.left = null
+        this.right = null
+    }
+}
+
+class BinarySearchTree {
+    constructor(val) {
+        this.roott = null
+    }
+}
+
+const tree = new BinarySearchTree()
+
+// Example 1: Manual insert:
+tree.root = new Node(10)
+tree.root.right  = new Node(15)
+tree.root.left = new Node(7)
+tree.root.left.right = new Node(9)
+console.log(tree)
+/* result:
+BinarySearchTree {
+  roott: null,
+  root: Node {
+    val: 10,
+    left: Node { val: 7, left: null, right: [Node] },
+    right: Node { val: 15, left: null, right: null }
+  }
+} */
