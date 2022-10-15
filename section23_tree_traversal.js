b18bc4a32ecd50f004f1c44840e936c880cdf39a
Roy Daniel <roydanielr@gmail.com>
2022-10-15 09:26:41 -0400

    tree traversal summary

---
+++ section23_tree_traversal.js
@@ -0,0 +1,24 @@
+/* Tree Traversal:
+==================
+Traversing a tree = visiting every node ONLY 1 time.
+There are many ways to do this, we will focus on 4:
+-   Breadth First Search (BFS)
+-   Depth First PreOrder
+-   Depth First PostOrder
+-   Depth First InOrder
+
+BFS is "horizontal" \ on the same level ----->
+SDF is "vertical" \ different level each time... 
+
+Example:
+========
+In order to find if 99 is in the tree below,
+We will need to travese the tree.
+
+    ________10_______
+    |               |
+____19____          6____
+|       |               |
+99      8               20
+
+*/
